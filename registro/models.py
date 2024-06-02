66d8fe1db7df8f7d8cead773d8838482ae254b82
EmilianoCelleri <emiliano.celleri@gmail.com>
2024-06-02 16:03:25 -0300

    Model medico, model especialidad

--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Especialidad(models.Model):
+    especialidad = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.especialidad
+        
+
+class Medico(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    especialidad = models.OneToOneField(Especialidad, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+    
+
